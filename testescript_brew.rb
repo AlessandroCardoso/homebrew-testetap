7b466b726fd8be1a53b002401a9805eafe31b3fb
acardosoi9 <a.cardoso@i9corp.com.br>
2020-04-17 17:21:41 -0300

    Teste de dependencia.

--- testescript_brew.rb
+++ testescript_brew.rb
@@ -3,6 +3,7 @@ class TestescriptBrew < Formula
     homepage ""
     url "https://github.com/AlessandroCardoso/testescript_brew/archive/0.0.1.tar.gz"
     sha256 "77d5b0bf6257a4009ece58ff0bb9d1703db8771b7571708140151c30c8f60098"
+    depends_on "dosunix"
   
     def install
         bin.install "testescript_brew"
