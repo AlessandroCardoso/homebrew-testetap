24bbcf2aee40fbe997a626cd831bf0071506e6ec
acardosoi9 <a.cardoso@i9corp.com.br>
2020-04-23 13:59:23 -0300

    Modificada versão i9corp-uid

--- i9corp-uid.rb
+++ i9corp-uid.rb
@@ -1,8 +1,7 @@
 class I9corpUid < Formula
     desc "I9Corp Application to Identify Device"
     homepage "http://www.i9corp.com.br"
-    url "https://github.com/AlessandroCardoso/i9corp-uid/archive/0.0.1.tar.gz"
-    sha256 "f01abf2b4b2f441b773637a8f5b29ee675bce38a2c73112a0fbc2850198d9d05"
+    url "https://github.com/AlessandroCardoso/i9corp-uid/archive/0.0.2.tar.gz"
     def install
         include.install "i9corp_entry_set.h", "i9corp_export.h", "i9corp_license_base.h", "i9corp_license.h", "i9corp_uid.h"
         lib.install "libi9corp-uid.a"
