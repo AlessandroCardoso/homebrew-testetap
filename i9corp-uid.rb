9da5d7d3b4fab88efaf3897bab2aa3e9826138d9
acardosoi9 <a.cardoso@i9corp.com.br>
2020-04-23 13:50:08 -0300

    Teste i9corp-uid.rb 3

--- i9corp-uid.rb
+++ i9corp-uid.rb
@@ -4,12 +4,8 @@ class I9corpUid < Formula
     url "https://github.com/AlessandroCardoso/i9corp-uid/archive/0.0.1.tar.gz"
     sha256 "f01abf2b4b2f441b773637a8f5b29ee675bce38a2c73112a0fbc2850198d9d05"
     def install
-        "/include/i9corp/structures/i9corp_entry_set.h"
-        "i9corp-uid/include/i9corp/uid/i9corp_export.h"
-        "/include/i9corp/uid/i9corp_license_base.h"
-        "/include/i9corp/uid/i9corp_license.h"
-        "/include/i9corp/uid/i9corp_uid.h"
-        "/lib/libi9corp-uid.a"
+        include.install "i9corp_entry_set.h", "i9corp_export.h", "i9corp_license_base.h", "i9corp_license.h", "i9corp_uid.h"
+        lib.install "libi9corp-uid.a"
     end
     test do
       system "false"
