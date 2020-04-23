d304f814ee321978304d612e13b76b5719291375
acardosoi9 <a.cardoso@i9corp.com.br>
2020-04-23 15:54:22 -0300

    Corrigido nome do i9corp-rest.rb

--- i9corp-rest.rb
+++ i9corp-rest.rb
@@ -1,4 +1,4 @@
-class I9corpUid < Formula
+class I9corpRest < Formula
     desc "I9Corp Biblioteca de requisição HTTP para voltada para o Mamute"
     homepage "http://www.i9corp.com.br"
     url "https://github.com/AlessandroCardoso/i9corp-rest/archive/0.0.1.tar.gz"
